937161424193427ac20da37a8a23a3cb79eaba77
Cristiam Herrera Daza <47664718+cristiamherreradaza@users.noreply.github.com>
2021-09-01 22:20:30 -0400

    Archivos iniciales del proyecto

---
+++ app/src/main/java/net/goghu/elisa/Constants.kt
@@ -0,0 +1,17 @@
+package net.goghu.elisa
+
+object Constants {
+
+    const val BASE_URL = "https://goghu.net/elisa/public"
+    const val API_PATH = "/api"
+    const val REGISTER_PATH = "/User/registra"
+
+    const val EMAIL_PARAM = "email"
+    const val PASSWORD_PARAM = "password"
+    const val NOMBRE_PARAM = "name"
+
+    const val ID_PROPERTY = "password"
+    const val TOKEN_PROPERTY = "token"
+
+    const val ERROR_VALUE = "fail"
+}
