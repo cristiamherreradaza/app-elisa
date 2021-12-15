8f131adeecf37fa1c44aaccf26958eeb7c98ac65
Cristiam Herrera <cristiamherrera@gmail.com>
2021-12-15 18:21:20 -0400

    Se adiciono las clases para el registro del usuario

--- app/src/main/java/net/goghu/elisa/RegistroActivity.kt
+++ app/src/main/java/net/goghu/elisa/RegistroActivity.kt
@@ -1,8 +1,14 @@
 package net.goghu.elisa
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import net.goghu.elisa.databinding.ActivityRegistroBinding
+import org.json.JSONObject
+import org.json.JSONTokener
 
 class RegistroActivity : AppCompatActivity() {
 
@@ -14,5 +20,83 @@ class RegistroActivity : AppCompatActivity() {
         binding = ActivityRegistroBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnRegistrar.setOnClickListener{
+
+            val nombre = binding.tiNombre.text.toString()
+            val email = binding.tiEmail.text.toString()
+            val password = binding.tiPassword.text.toString()
+
+            if (nombre.isEmpty()){
+                binding.tiNombre.error = getString(R.string.card_required)
+            }
+
+            if (email.isEmpty()){
+                binding.tiEmail.error = getString(R.string.card_required)
+            }
+
+            if (password.isEmpty()){
+                binding.tiPassword.error = getString(R.string.card_required)
+            }
+
+            envia(nombre, email, password)
+        }
+    }
+
+    private fun envia(nombre:String, email: String, password: String){
+//        Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show()
+        val url = Constants.BASE_URL + Constants.API_PATH + Constants.REGISTER_PATH
+
+        val name = binding.tiNombre.text.toString().trim()
+        val email = binding.tiEmail.text.toString().trim()
+        val password = binding.tiPassword.text.toString().trim()
+
+        val jsonParams = JSONObject()
+
+        jsonParams.put(Constants.EMAIL_PARAM, email)
+        jsonParams.put(Constants.NOMBRE_PARAM, name)
+        jsonParams.put(Constants.PASSWORD_PARAM, password)
+
+        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.POST, url, jsonParams, { response ->
+//            me retorna los datos de guardado
+            Log.i("respuesta", response.toString())
+
+            // manejamos la respuesta
+            val jsonObject = JSONTokener(response.toString()).nextValue() as JSONObject
+            // capturamos el id que nos devolvio el registro
+            val usuario = jsonObject.getString("usuario")
+//            Log.i("Usuario: ", usuario)
+
+            // Obtenemos el PreferenceManager
+            val preferencias  = getPreferences(Context.MODE_PRIVATE);
+
+            with(preferencias.edit()){
+                putString("usuario", usuario)
+                putString("nombre", name)
+                putString("email", email)
+                    .apply()
+            }
+
+            Log.i("nombre g ", preferencias.getString("nombre", "NA").toString())
+
+//            preferencias.getString()
+
+//            updateUI("Se registro correctamente")
+            
+
+        },{
+            if (it.networkResponse.statusCode == 400){
+//                updateUI("error en la peticion")
+            }
+        }){
+            override fun getHeaders(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["Content-Type"] = "application/json"
+                return params
+            }
+        }
+
+        LoginApplication.reqResApi.addToRequestQueue(jsonObjectRequest)
+
     }
 }
